1a556dd339a905670235fc21baaf47d574072aae
Luis Egan <luisdegan10@hotmail.com>
2018-02-19 20:39:01 -0300

    fix routing bug

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,22 +1,23 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
 
-import Routes from '../config/routes';
-import Header from './globals/Header';
+import Routes from "../config/routes";
+import Header from "./globals/Header";
 
 class App extends Component {
   render() {
     return (
-      <div className='App'>
-        <Header/>
-        <div className='Page'>
-          <Routes/>
+      <div className="App">
+        <Header />
+        <div className="Page">
+          <Routes />
         </div>
       </div>
     );
   }
 }
 
-const mapStateToProps = state => (state);
+const mapStateToProps = state => state;
 
-export default connect(mapStateToProps)(App);
+export default withRouter(connect(mapStateToProps)(App));
